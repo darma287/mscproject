434c960ddc237163f218f478c8e65cec0bb971cf
Darma Paramarta <darmaparamarta@gmail.com>
2024-07-11 13:36:12 +0100

    [Feature] Search Bar Front end working

---
+++ shared/Data.js
@@ -0,0 +1,33 @@
+const SportList =[
+    {
+        id:1,
+        name: 'Football',
+        image:'',
+    },
+    {
+        id:2,
+        name: 'Basketball',
+        image:'',
+    },
+    {
+        id:3,
+        name: 'Volleyball',
+        image:'',
+    },
+    {
+        id:4,
+        name: 'Tennis',
+        image:'',
+    },
+    {
+        id:5,
+        name: 'Badminton',
+        image:'',
+    },
+    {
+        id:6,
+        name: 'Rugby',
+        image:'',
+    },
+
+]
