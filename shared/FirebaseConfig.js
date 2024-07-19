9dd3c2b78780b8e8d1860ef7e88dfd26604591bf
Darma Paramarta <darmaparamarta@gmail.com>
2024-07-20 00:24:54 +0100

    Fetch firebase data

--- shared/FirebaseConfig.js
+++ shared/FirebaseConfig.js
@@ -1,11 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDuLXpubNn51RYvp0DmM4wAoLPS4pF1rW0",
   authDomain: "sportapp-4d235.firebaseapp.com",
@@ -18,4 +15,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+if (typeof window !== 'undefined') {
+  const analytics = getAnalytics(app);
+}
+
+export default app;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,33 @@
+import { useEffect } from 'react';
 import Hero from '../../components/Home/Hero';
 import Search from '../../components/Home/Search';
 import SportList from '../../components/Home/SportList';
-import { Inter } from 'next/font/google'
-const inter = Inter({ subsets: ['latin'] })
+import app from '../../shared/FirebaseConfig';
+import { collection, getDocs } from "firebase/firestore";
+import { getFirestore } from 'firebase/firestore';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+const db = getFirestore(app);
+
+const getPost = async () => {
+  const querySnapshot = await getDocs(collection(db, "post"));
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, " => ", doc.data());
+  });
+};
 
 export default function Home() {
+  useEffect(() => {
+    getPost();
+  }, []);
+
   return (
     <main className="p-5 sm:px-7 md:px-10">
-<Hero />
-<Search />
-<SportList />
-
+      <Hero />
+      <Search />
+      <SportList />
     </main>
   );
 }
-
