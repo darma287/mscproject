e619fb4a75a598af6a6ea3d45ef12f0c2b8db3b1
Darma Paramarta <darmaparamarta@gmail.com>
2024-07-31 17:31:47 +0100

    [Feature] User can delete their post]

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,9 +1,9 @@
-import { collection, getDocs, getFirestore, query, where } from 'firebase/firestore';
-import { useSession } from 'next-auth/react'
-import React, { useEffect } from 'react'
+import { collection, getDocs, getFirestore, query, doc, where } from 'firebase/firestore';
+import { useSession } from 'next-auth/react';
+import React, { useEffect, useState } from 'react';
 import app from '../../../shared/FirebaseConfig';
-import { useState } from 'react';
 import PostItem from '../../../components/Home/PostItem';
+import { deleteDoc } from 'firebase/firestore';
 
 function Profile() {
     const { data: session } = useSession();
@@ -22,19 +22,30 @@ function Profile() {
             const querySnapshot = await getDocs(q);
             const posts = [];
             querySnapshot.forEach((doc) => {
-                posts.push(doc.data());
+                let data = doc.data();
+                data.id = doc.id; // Include the id in the post data
+                posts.push(data); // Push the post data including the id
             });
             setUserPost(posts);
         }
     };
 
+    const onDeletePost =async (id) => {
+        console.log(id);
+        await deleteDoc(doc(db,"post",id))
+        setUserPost(userPost.filter(post => post.id !== id));
+    }
+
     return (
         <div className='p-6 mt-8 lg:w-[35%] md:w-[100%]'>
             <h2 className='text-[30-px] font-extrabold text-blue-500'>Profile</h2>
             <p>Manage your posts</p>
-            <div>
+            <div className="p-5 sm:px-7 md:px-10">
                 {userPost && userPost.map((item, index) => (
-                    <PostItem key={index} post={item} />
+                    <div key={index}>
+                        <PostItem post={item} />
+                        <button onClick={() => onDeletePost(item.id)}>Delete</button>
+                    </div>
                 ))}
             </div>
         </div>
