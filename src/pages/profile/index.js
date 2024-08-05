29b578ea35adf4cf4061416ad5a16b2f14a5b6ba
Darma Paramarta <darmaparamarta@gmail.com>
2024-08-06 00:37:54 +0100

    [UI, BugFix] Fix profile page UI and delete post bug fix

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -3,87 +3,109 @@ import { useSession } from 'next-auth/react';
 import React, { useEffect, useState } from 'react';
 import app from '../../../shared/FirebaseConfig';
 import PostItem from '../../../components/Home/PostItem';
-import Posts from '../../../components/Home/Posts';
 
 function Profile() {
-    const { data: session } = useSession();
-    const [userPost, setUserPost] = useState([]);
-    const [joinedPosts, setJoinedPosts] = useState([]); // State for joined posts
-    const db = getFirestore(app);
+  const { data: session } = useSession();
+  const [userPosts, setUserPosts] = useState([]);
+  const [joinedPosts, setJoinedPosts] = useState([]);
+  const db = getFirestore(app);
 
-    useEffect(() => {
-        if (session) {
-            getUserPost();
-            getJoinedPosts(); // Fetch joined posts
-        }
-    }, [session]);
+  useEffect(() => {
+    if (session) {
+      getUserPosts();
+      getJoinedPosts();
+    }
+  }, [session]);
 
-    const getUserPost = async () => {
-        if (session?.user?.email) {
-            const q = query(collection(db, 'post'), where('email', '==', session.user.email));
-            const querySnapshot = await getDocs(q);
-            const posts = [];
-            querySnapshot.forEach((doc) => {
-                let data = doc.data();
-                data.id = doc.id; // Include the id in the post data
-                posts.push(data); // Push the post data including the id
-            });
-            setUserPost(posts);
-        }
-    };
+  const getUserPosts = async () => {
+    if (session?.user?.email) {
+      const q = query(collection(db, 'post'), where('email', '==', session.user.email));
+      const querySnapshot = await getDocs(q);
+      const posts = [];
+      querySnapshot.forEach((doc) => {
+        let data = doc.data();
+        data.id = doc.id;
+        posts.push(data);
+      });
+      setUserPosts(posts);
+    }
+  };
 
-    const getJoinedPosts = async () => { // Function to fetch joined posts
-        if (session?.user?.email) {
-            const q = query(collection(db, 'joinedPosts'), where('userEmail', '==', session.user.email));
-            const querySnapshot = await getDocs(q);
-            const posts = [];
-            querySnapshot.forEach((doc) => {
-                let data = doc.data();
-                data.id = doc.id; // Include the id in the post data
-                posts.push(data); // Push the post data including the id
-            });
-            setJoinedPosts(posts); // Set the state with the joined posts
-        }
-    };
+  const getJoinedPosts = async () => {
+    if (session?.user?.email) {
+      const q = query(collection(db, 'joinedPosts'), where('userEmail', '==', session.user.email));
+      const querySnapshot = await getDocs(q);
+      const posts = [];
+      querySnapshot.forEach((doc) => {
+        let data = doc.data();
+        data.id = doc.id;
+        posts.push(data);
+      });
+      setJoinedPosts(posts);
+    }
+  };
 
-    const onDeletePost = async (id) => {
-        console.log(id); // Log the id of the post to be deleted
-        await deleteDoc(doc(db, "post", id)); // Delete the document with the specified id
-        setUserPost(userPost.filter(post => post.id !== id)); // Update the state to remove the deleted post
-    };
+  const onDeletePost = async (id) => {
+    await deleteDoc(doc(db, "post", id));
+    setUserPosts(userPosts.filter(post => post.id !== id));
+    await deleteDoc(doc(db, "joinedPosts", id));
+    setJoinedPosts(userPosts.filter(post => post.id !== id));
+  };
 
-    const onJoinPost = async (post) => { // Function to join a post
-        console.log("onJoinPost called with post:", post);
-        if (session?.user?.email) {
-            const joinedPost = { ...post, userEmail: session.user.email }; // Add user email to the joined post
-            await setDoc(doc(db, "joinedPosts", `${post.id}-${session.user.email}`), joinedPost); // Add joined post to Firestore
-            setJoinedPosts([...joinedPosts, joinedPost]); // Update the state to include the new joined post
-        }
-    };
+  const onJoinPost = async (post) => {
+    if (session?.user?.email) {
+      const joinedPost = { ...post, userEmail: session.user.email };
+      await setDoc(doc(db, "joinedPosts", `${post.id}-${session.user.email}`), joinedPost);
+      setJoinedPosts([...joinedPosts, joinedPost]);
+    }
+  };
 
-    console.log("Profile onJoinPost:", onJoinPost);
-
-    return (
-        <div className='p-6 mt-8 lg:w-[35%] md:w-[100%]'>
-            <h2 className='text-[30-px] font-extrabold text-blue-500'>Profile</h2>
-            <p>Manage your posts</p>
-            <div className="p-5 sm:px-7 md:px-10">
-                <h3>Your Posts</h3> {/* Section for user's own posts */}
-                {userPost && userPost.map((item, index) => (
-                    <div key={index}>
-                        <PostItem post={item} />
-                        <button onClick={() => onDeletePost(item.id)}>Delete</button>
-                    </div>
-                ))}
-                <h3>Joined Sports</h3> {/* Section for joined posts */}
-                {joinedPosts && joinedPosts.map((item, index) => (
-                    <div key={index}>
-                        <PostItem post={item} />
-                    </div>
-                ))}
-            </div>
+  return (
+    <div className="p-6 mt-8 lg:w-[70%] md:w-full mx-auto bg-background text-forGrey-900">
+      <div className="p-6 bg-white shadow-sm rounded-lg mb-8">
+        <h2 className="text-2xl font-extrabold text-primary-500 mb-4">Profile</h2>
+        <img src={session?.user?.image} alt="User Picture" className="w-16 h-16 rounded-full mr-4" />
+          <div>
+            <p className="text-lg"><strong>Name:</strong> {session?.user?.name}</p>
+            <p className="text-lg"><strong>Email:</strong> {session?.user?.email}</p>
+            <p className="text-lg"><strong>Gender:</strong> {session?.user?.gender}</p>
+            <p className="text-lg"><strong>Date of Birth:</strong> {session?.user?.dob}</p>
+          </div>
+      </div>
+      <div className="p-5 sm:px-7 md:px-10">
+        <div className="mb-8">
+          <h3 className="text-xl font-bold mb-3">Your Posts</h3>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {userPosts && userPosts.map((item, index) => (
+              <div key={index} className="bg-white shadow-sm rounded-lg">
+                <div className="p-4">
+                  <PostItem post={item} />
+                  <button
+                    onClick={() => onDeletePost(item.id)}
+                    className="mt-2 px-4 py-2 bg-secondary-500 text-white rounded hover:bg-secondary-700 w-full"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-    );
+        <div>
+          <h3 className="text-xl font-bold mb-3">Joined Sports</h3>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {joinedPosts && joinedPosts.map((item, index) => (
+              <div key={index} className="bg-white shadow-sm rounded-lg">
+                <div className="p-4">
+                  <PostItem post={item} />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
